a9d891eba3535c3009742c58586ad32df2d65ac0
puthsovandara <puthsovannara@gmail.com>
2026-03-17 17:27:07 +0700

    add-cors

--- QuickTable.API/QuickTable.API/Program.cs
+++ QuickTable.API/QuickTable.API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://localhost:5174",
                  "http://localhost:5175",
-                "https://quick-order.site"
+                "https://quicktable.store"
             )
             .AllowAnyMethod()
             .AllowAnyHeader()
