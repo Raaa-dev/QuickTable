d3892729acaebf091dc6d2e73e08d728ef600ea0
puthsovandara <puthsovannara@gmail.com>
2026-06-12 10:16:51 +0700

    fix-pagination

--- QuickTable.API/QuickTable.Service/Helpers/BaseQueryFilter.cs
+++ QuickTable.API/QuickTable.Service/Helpers/BaseQueryFilter.cs
@@ -8,7 +8,19 @@ namespace QuickTable.Service.Helpers
 {
     public class BaseQueryFilter
     {
-        public int PageNo { get; set; } = 1;
-        public int PageSize { get; set; } = 300;
+        private int _pageNo = 1;
+        private int _pageSize = 10;
+
+        public int PageNo
+        {
+            get => _pageNo;
+            set => _pageNo = value <= 0 ? 1 : value;
+        }
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value <= 0 ? 10 : value;
+        }
     }
 }
